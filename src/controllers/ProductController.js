452b8f5fa6e7a70e9006f21ef41181dc482c7d94
DESKTOP-CJJ78KK\Marlon <marlon.rp94@gmail.com>
2021-09-27 19:27:05 -0300

    docker-compose

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,23 +1,31 @@
 const mongoose = require('mongoose');
+const redis = require('promise-redis')();
+const client = redis.createClient();
 
 const Product = mongoose.model('Product');
 
 module.exports = {
-    async index(req, res) {
+    async list(req, res) {
         const { page = 1 } = req.query;
         const { limit = 10 } = req.query;
         const products = await Product.paginate({}, { page, limit: parseInt(limit) });
         return res.json(products);
     },
-    async show(req, res) {
+    async getById(req, res) {
+        const result = await client.get(req.params.id);
+        if (result) {
+            return res.json(JSON.parse(result));
+        }
+
         const product = await Product.findById(req.params.id);
+        await client.set(req.params.id, JSON.stringify(product), 'EX', 30);
         return res.json(product);
     },
-    async store(req, res) {
+    async save(req, res) {
         const product = await Product.create(req.body);
         return res.json(product);
     },
-    async update(req, res) {
+    async edit(req, res) {
         const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
         return res.json(product);
     },
